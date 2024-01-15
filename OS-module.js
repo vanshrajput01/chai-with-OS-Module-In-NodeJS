78b522150ffcb50be444a3b144925cfc581285b7
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-15 18:16:35 +0530

    os methrod in Node.js

--- OS-module.js
+++ OS-module.js
@@ -1,4 +1,16 @@
 // Operating System Module
 
 const os = require("os")
-console.log(os);
+// console.log(os);
+
+console.log("CPU Architecture : ",os.arch()); // x64
+
+// this methrod give result in Bytes
+
+console.log("Free Memory : ",os.freemem()); 
+
+console.log("Total Memory : ",os.totalmem()); 
+
+console.log("Network interfaces : ",JSON.stringify(os.networkInterfaces()));
+
+console.log("Operating system default dir for temps files : ",os.tmpdir()); 
